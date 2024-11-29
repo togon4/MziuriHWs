7617b99a2c65fcb7a132968bd3d94a0104e930eb
pc <iakobtogonidze123@gmail.com>
2024-11-29 11:54:33 -0800

    commit

--- src/HW1/Main.java
+++ src/HW1/Main.java
@@ -2,9 +2,9 @@ package HW1;
 
 public class Main {
     public static void main(String[] args) {
-        City city1 = new City("CityA", 500000, 600);
-        City city2 = new City("CityB", 300000, 400);
-        City city3 = new City("CityC", 200000, 300);
+        City city1 = new City("Tbilisi", 500000, 600);
+        City city2 = new City("Qutaisi", 300000, 400);
+        City city3 = new City("Telavi", 200000, 300);
 
         City[] cities = {city1, city2, city3};
 
