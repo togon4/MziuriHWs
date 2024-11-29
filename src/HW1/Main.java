b4fa3408318871680e209902d6e1cb4e74cad4ad
pc <iakobtogonidze123@gmail.com>
2024-11-29 12:00:54 -0800

    commit second

--- src/HW1/Main.java
+++ src/HW1/Main.java
@@ -2,7 +2,7 @@ package HW1;
 
 public class Main {
     public static void main(String[] args) {
-        City city1 = new City("Tbilisi", 1000000, 13000);
+        City city1 = new City("Tbilisi", 1000000, 1300);
         City city2 = new City("Qutaisi", 300000, 400);
         City city3 = new City("Telavi", 200000, 300);
 
