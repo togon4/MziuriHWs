8f004946fe23b545f8b20fbb0c7b594ccec5ebc9
pc <iakobtogonidze123@gmail.com>
2025-02-16 00:23:53 -0800

    commit

--- src/HW36/StringReturnFunction.java
+++ src/HW36/StringReturnFunction.java
@@ -1,10 +1,11 @@
 package HW36;
 import java.util.function.Function;
 
-public class StringReturnFunction implements Function<String, String> {
+class StringReturnFunction implements Function<String, String> {
     @Override
     public String apply(String input) {
-        return input;
+        return new StringBuilder(input).reverse().toString();
     }
 }
+
 
